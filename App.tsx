102f50b00f337fdf0e8378020ae2335ba4172996
Nicollas <nicollasramires@gmail.com>
2022-10-10 23:46:59 -0300

    Progress in loading/home page

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 import { StatusBar } from 'react-native';
 import { Background } from './src/components/Background';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black } from '@expo-google-fonts/inter';
-//import { Home }
+import { Home } from './src/screens/Home';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -15,11 +16,11 @@ export default function App() {
     <Background>
       <StatusBar 
         barStyle="light-content" 
-        backgroundColor='transparent' 
+        backgroundColor="transparent" 
         translucent
       />
 
-    
+      {fontsLoaded ? <Home/> : <Loading/>}
     </Background>
   );
 }
