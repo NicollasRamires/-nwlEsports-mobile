38135ceaa4595d5f31655438fa78a154c862cbca
Nicollas <nicollasramires@gmail.com>
2022-10-16 18:59:41 -0300

    GameCard in Homescreen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, FlatList } from 'react-native';
 import { styles } from './styles';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
+import { Heading } from '../../components/Heading';
+import { GameCard } from '../../components/GameCard';
 
+import { GAMES } from '../../utils/games';
+ 
 export function Home(){
     return (
         <View style={styles.container}>
             <Image source={logoImg} style={styles.logo}/>
+            
+            <Heading title="Encontre seu duo" subtitle="Selecione o game que deseja jogar...">
+
+            </Heading>
+
+            <FlatList   horizontal 
+                        showsHorizontalScrollIndicator={false}
+                        contentContainerStyle={styles.contenteList}
+                        data={GAMES} 
+                        keyExtractor={item => item.id} 
+                        renderItem={({ item }) => (
+                                <GameCard data={ item }>
+                                </GameCard>       
+                        )}>
+
+            </FlatList>
+
+                
         </View>
     );
 }
