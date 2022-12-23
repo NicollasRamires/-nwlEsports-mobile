042bf613dc8defaa408ae55a2cb398af017990e8
Nicollas Ramires <nicollasramires@gmail.com>
2022-12-22 22:39:59 -0300

    get games from api

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,11 +3,19 @@ import { styles } from './styles';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
 import { Heading } from '../../components/Heading';
-import { GameCard } from '../../components/GameCard';
+import { GameCard, GameCardProps } from '../../components/GameCard';
 
-import { GAMES } from '../../utils/games';
+import { useEffect, useState } from 'react';
  
 export function Home(){
+    const [games, setGames] = useState<GameCardProps[]>([]);
+    
+    useEffect(()=>{
+        fetch('http://192.168.100.10:3333/games')
+        .then(response => response.json())
+        .then(data => setGames(data))
+    }, [])
+
     return (
         <View style={styles.container}>
             <Image source={logoImg} style={styles.logo}/>
@@ -19,7 +27,7 @@ export function Home(){
             <FlatList   horizontal 
                         showsHorizontalScrollIndicator={false}
                         contentContainerStyle={styles.contenteList}
-                        data={GAMES} 
+                        data={games} 
                         keyExtractor={item => item.id} 
                         renderItem={({ item }) => (
                                 <GameCard data={ item }>
